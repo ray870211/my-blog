3c1f627e0b00c77908a0b1325f04b824dac772ff
ray870211 <ray870211@gmail.com>
2025-06-08 12:54:16 +0800

    feat: add blog content configuration

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// src/content/content.config.ts
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+
+
+export const collections = {
+  blog,
+
+};
